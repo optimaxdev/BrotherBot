a160175099abbca4b90d1af29befb302f0ea1a32
Nikolai <imbpp123@gmail.com>
2019-03-04 01:13:26 +0400

    Unique assignee+status check

--- app/jiraissues.py
+++ app/jiraissues.py
@@ -12,17 +12,20 @@ def check_issue_unique():
         send_message("Чтото ничего не нашел я по запросу и это уже зашквар. Проверь сам:\n%s" % DEFAULT_UVP_JQL)
 
     unique_index = {}
-    for id, item in collection.get_collection().items():
+    for item_id, item in collection.get_collection().items():
         index = '_'.join([item.get_assignee().get_id(), item.get_status().get_id()])
         if unique_index.get(index) is None:
             unique_index[index] = [item]
         else:
             unique_index[index].append(item)
-        unique_index[index].append(item)
 
-    for id, item in unique_index.items():
+    for item_id, item in unique_index.items():
         if len(item) > 1:
             message = 'Найдено нарушение на борде по правилу 1 задача на 1 человека в определенном статусе'
             for issue in item:
-                message = message + '\n%s со статусом %s, исполнитель %s' % (issue.get_key(), issue.get_status().get_name(), issue.get_assignee().get_display_name())
+                message = message + '\n%s статус %s, исполнитель %s' % (
+                    issue.get_key(),
+                    issue.get_status().get_name(),
+                    issue.get_assignee().get_display_name()
+                )
             send_message(message)
