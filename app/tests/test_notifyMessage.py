2b6cd7bb3c266d85179740a69722f0ec7bd8324e
Nikolay Kornushkov <dev@glassesusa.com>
2019-03-24 15:41:33 +0400

    Fix tests

--- app/tests/test_notifyMessage.py
+++ app/tests/test_notifyMessage.py
@@ -10,9 +10,3 @@ class TestNotifyMessage(TestCase):
             item._get_template([])
         with self.assertRaises(NotImplementedError):
             item._get_data()
-
-    def test__get_data(self):
-        self.fail()
-
-    def test_report(self):
-        self.fail()
